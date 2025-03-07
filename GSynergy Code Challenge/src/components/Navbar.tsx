8ada0a7bc44e93c9a183b9c0103b42df6a74b9e3
shaukinkhan477 <shaukinkhan477@gmail.com>
2025-03-07 11:30:35 +0530

    added navbar,sidebar,and routing

--- GSynergy Code Challenge/src/components/Navbar.tsx
+++ GSynergy Code Challenge/src/components/Navbar.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import "./Navbar.css";
+import logo from "../assets/gsynergy_logo.svg";
 
 
 
 
 function Navbar() {
 
+  const [authenticated, setAuthenticated] = React.useState(false);
+
+  const handleAuthClick = () => {
+    setAuthenticated(!authenticated);
+  };
+
   return (
-     <></>
-   )
+    <>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <img src={logo} alt="Company Logo" className="logo" />
+          <span className="app-title">Data Viewer App</span>
+        </div>
+        <div className="navbar-right">
+          <button onClick={handleAuthClick}>
+            {authenticated ? "Sign Out" : "Sign In"}
+          </button>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
--- GSynergy Code Challenge/src/components/Sidebar.tsx
+++ GSynergy Code Challenge/src/components/Sidebar.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-
-
-
+import { NavLink } from "react-router-dom";
+import { FaStore, FaTags, FaTable, FaChartBar } from "react-icons/fa";
+import "./Sidebar.css";
 
 function Sidebar() {
-
   return (
-     <></>
-   )
+    <>
+      <aside className="sidebar">
+        <NavLink to="/stores" className="nav-link">
+          <FaStore className="icon" />
+          <span>Stores</span>
+        </NavLink>
+        <NavLink to="/skus" className="nav-link">
+          <FaTags className="icon" />
+          <span>SKUs</span>
+        </NavLink>
+        <NavLink to="/planning" className="nav-link">
+          <FaTable className="icon" />
+          <span>Planning</span>
+        </NavLink>
+        <NavLink to="/chart" className="nav-link">
+          <FaChartBar className="icon" />
+          <span>Chart</span>
+        </NavLink>
+      </aside>
+    </>
+  );
 }
 
 export default Sidebar;
